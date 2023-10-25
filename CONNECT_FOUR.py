29c4ea36cb57d2610a69537f16d62569b920690d
Nate <np.sweany@gmail.com>
2023-10-24 19:54:58 -0600

    Major improvement, fixing AB function

--- CONNECT_FOUR.py
+++ CONNECT_FOUR.py
@@ -7,11 +7,10 @@ WINDOW_SIZE = 4
 OFFSET = WINDOW_SIZE - 1
 EMPTY = 0
 SWAP_PLAYER = 3
-CENTER_COLUMN = COLUMN_COUNT // 2
-CENTER_COLUMN_MULTIPLIER = 100
+CENTER_COLUMN_MULTIPLIER = 3
 
 # SET THIS FOR ALPHA-BETA SEARCH DEPTH
-ALPHA_BETA_DEPTH_LIMIT = 4
+ALPHA_BETA_DEPTH_LIMIT = 5
 
 
 
@@ -79,18 +78,22 @@ def is_draw(grid):
 
 def score_window(window, player):
     score = 0
-    opp_player = SWAP_PLAYER - player
+    opp_player = PLAYER
+    if player == PLAYER:
+        opp_player = OPP_PLAYER
+    
 
     # (Scoring self-moves)
     if list(window).count(player) == 4:
-        score += 10
-    elif list(window).count(player) == 3 and list(window).count(EMPTY) == 1:
+        score += 100
+    if list(window).count(player) == 3 and list(window).count(EMPTY) == 1:
         score += 5
-    elif list(window).count(player) == 2 and list(window).count(EMPTY) == 2:
+    if list(window).count(player) == 2 and list(window).count(EMPTY) == 2:
         score += 2
     # Blocking (Scoring opp-moves)
     if list(window).count(opp_player) == 3 and list(window).count(EMPTY) == 1:
-        score -= 40
+        score -= 4
+
 
     return score
 
@@ -104,6 +107,12 @@ def score_heuristic(grid, player):
     center_column_count = center_array.count(player)
     score += center_column_count * CENTER_COLUMN_MULTIPLIER
 
+    # Score Horizontal
+    for row in range(ROW_COUNT):
+        row_arr = [int(i) for i in list(grid[row,:])]
+        for col in range(COLUMN_COUNT - OFFSET):  
+            window = row_arr[col:col+WINDOW_SIZE]
+            score += score_window(window, player)
 
     # Score Vertical
     for col in range(COLUMN_COUNT):  
@@ -111,11 +120,7 @@ def score_heuristic(grid, player):
             window = grid[row:row+WINDOW_SIZE, col]
             score += score_window(window, player)
 
-    # Score Horizontal
-    for row in range(ROW_COUNT):
-        for col in range(COLUMN_COUNT - OFFSET):  
-            window = grid[row, col:col+WINDOW_SIZE]
-            score += score_window(window, player)
+
 
     # Score Positive Slope Diagonal
     for row in range(ROW_COUNT - OFFSET):
@@ -128,7 +133,7 @@ def score_heuristic(grid, player):
         for col in range(OFFSET, COLUMN_COUNT):
             window = [grid[row+i, col-i] for i in range(WINDOW_SIZE)]
             score += score_window(window, player)
-            
+    
     return score
 
 
@@ -136,49 +141,47 @@ def score_heuristic(grid, player):
 
 
 
-def alphabeta(grid, depth, alpha, beta, maximizing_player, player):
-    if depth == 0 or is_end_of_game(grid, player) or is_end_of_game(grid, SWAP_PLAYER-player) or is_draw(grid) or len(get_valid_moves(grid)) == 0:
-        if is_end_of_game(grid, player):
-            return 100000
-        if is_end_of_game(grid, SWAP_PLAYER-player):
-            return -100000
-        else:
-            return score_heuristic(grid, player)
+def alphabeta(grid, depth, alpha, beta, maximizing_player):
+    if depth == 0:
+        return(None, score_heuristic(grid, PLAYER))
+    if is_end_of_game(grid, PLAYER):
+        return(None, 1000000)
+    if is_end_of_game(grid, OPP_PLAYER):
+        return(None, -1000000)
+    if is_draw(grid):
+        return (None, 0)
+    if len(get_valid_moves(grid)) == 0:
+        return(None, 0)
+
 
     if maximizing_player:
         max_score = float('-inf')
         for col in range(COLUMN_COUNT):
             if is_valid_move(grid, col):
-                new_grid = make_move(grid.copy(), col, player) # Ensuring to make a deep copy of the grid.
-                score = alphabeta(new_grid, depth-1, alpha, beta, False, SWAP_PLAYER-player)              
-                max_score = max(max_score, score)
-                alpha = max(alpha, score)
-                if beta <= alpha:
+                new_grid = make_move(grid.copy(), col, PLAYER) # Ensuring to make a deep copy of the grid.
+                new_score = alphabeta(new_grid, depth-1, alpha, beta, False)[1]             
+                if new_score > max_score:
+                    max_score = new_score
+                    column = col
+                alpha = max(alpha, new_score)
+                if alpha >= beta:
                     break
-        return max_score
+        return column, max_score
     else: #Minimizing player
         min_score = float('inf')
         for col in range(COLUMN_COUNT):
             if is_valid_move(grid, col):
-                new_grid = make_move(grid.copy(), col, player) # Ensuring to make a deep copy of the grid.
-                score = alphabeta(new_grid, depth-1, alpha, beta, True, SWAP_PLAYER-player)
-                min_score = min(min_score, score)
-                beta = min(beta, score)
-                if beta <= alpha:
+                new_grid = make_move(grid.copy(), col, OPP_PLAYER) # Ensuring to make a deep copy of the grid.
+                new_score = alphabeta(new_grid, depth-1, alpha, beta, True)[1]
+                if new_score < min_score:
+                    min_score = new_score
+                    column = col
+                beta = min(beta, new_score)
+                if alpha >= beta:
                     break
-        return min_score
+        return column, min_score
 
-def find_best_move(grid, player):
-    best_score = float('-inf')
-    best_col = -1
-    valid_moves = get_valid_moves(grid) # A list of all the possible moves we can make.
-    for col in valid_moves:
-        new_grid = make_move(grid.copy(), col, player) # Deep copy the grid.
-        move_score = alphabeta(new_grid, ALPHA_BETA_DEPTH_LIMIT, float('-inf'), float('inf'), True, player)
-        if move_score > best_score:
-            best_score = move_score
-            best_col = col
-    return best_col
+
 
 def main():
     print('Connect Four with Alpha-Beta Pruning in Python', file=sys.stderr)
@@ -194,12 +197,16 @@ def main():
         COLUMN_COUNT = int(json_data["width"])
         global CENTER_COLUMN
         CENTER_COLUMN = COLUMN_COUNT // 2
-        print(f"Row Count is: {ROW_COUNT}, Column Count is: {COLUMN_COUNT}, Center Column is: {CENTER_COLUMN}", file=sys.stderr)
+        global PLAYER
+        PLAYER = int(json_data["player"])
+        global OPP_PLAYER
+        OPP_PLAYER = SWAP_PLAYER - PLAYER
+
         grid = np.array(json_data["grid"], dtype=int).T #Transpose to get out of column-major format.
         print(grid, file=sys.stderr)
-        move = find_best_move(grid, json_data["player"])
+        col, _ = alphabeta(grid, ALPHA_BETA_DEPTH_LIMIT, float('-inf'), float('inf'), True)
         
-        action = {"move": move}
+        action = {"move": col}
         
         action_json = json.dumps(action)
 
